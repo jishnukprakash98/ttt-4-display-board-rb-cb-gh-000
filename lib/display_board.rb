548f1c4aa7c207548ea5f4ef9ecf191fad116a9d
jishnu prakasan <jishnukprakash.98@gmail.com>
2019-08-18 09:34:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "\n"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n"
+  
   puts "-----------"
   puts "\n"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
