ac09ece6690c5a9528417cf4d990cc184005a8cf
jishnu prakasan <jishnukprakash.98@gmail.com>
2019-08-18 09:34:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n"
-  
+
   puts "-----------"
   puts "\n"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
